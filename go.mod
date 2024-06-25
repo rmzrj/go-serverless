aab8b4888796e60aa3d53ac5fda9715f744c4639
ramees <rameesraj534@gmail.com>
2024-06-26 01:23:40 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/rmzrj/go-serverless
+
+go 1.22.4
+
+require github.com/aws/aws-sdk-go v1.54.7
+
+require (
+	github.com/aws/aws-lambda-go v1.47.0
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
---
+++ pkg/user/user.go
@@ -0,0 +1,56 @@
+package user
+
+import (
+	"errors"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+)
+
+var (
+	ErrorFailedToFetchRecord     = "failed to fetch record"
+	ErrorFailedToUnMarshalRecord = "failed to unmarshal"
+)
+
+type User struct {
+	Email     string `json:"email"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
+
+func FetchUser(email, tableName string, dynaClient dynamodbiface.DynamoDBAPI) (*User, error) {
+	input := &dynamodb.GetItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"email": {
+				S: aws.String(email),
+			},
+		},
+		TableName: aws.String(tableName),
+	}
+	result, err := dynaClient.GetItem(input)
+	if err != nil {
+		return nil, errors.New(ErrorFailedToFetchRecord)
+	}
+
+	item := new(User)
+	err = dynamodbattribute.UnmarshalMap(result.Item, item)
+
+	if err != nil {
+		return nil, errors.New(ErrorFailedToUnMarshalRecord)
+	}
+
+	return item, nil
+
+}
+
+func FetchUsers() {
+
+}
+
+func CreateUser() {}
+
+func UpdateUser() {}
+
+func DeleteUser() error {}
